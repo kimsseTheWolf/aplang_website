9803af62e8920f50a62e668d6403e5370b67677c
kimsseTheWolf <ilovemcmrcreeper@gmail.com>
2025-04-23 19:30:16 -0400

    Finish Modal Component

---
+++ src/components/Modal.vue
@@ -0,0 +1,115 @@
+<script setup>
+import { onMounted, ref, defineProps, watch, computed } from 'vue';
+import { animate } from 'animejs';
+import OverlayBox from './OverlayBox.vue';
+import RoundButton from './RoundButton.vue';
+
+const isShowingModal = defineModel("show", {
+    type: Boolean,
+    default: false,
+    required: true,
+});
+
+const props = defineProps({
+    showSidebar: {
+        type: Boolean,
+        default: false,
+    },
+    sidebarWidth: {
+        type: String,
+        default: "300px",
+    },
+    mainWidth: {
+        type: String,
+        default: "600px",
+    },
+    height: {
+        type: String,
+        default: "450px",
+    },
+});
+
+const adjustableCornerRadisu = computed(() => {
+    if (props.showSidebar) {
+        return "0px"
+    }
+    else {
+        return "7px"
+    }
+});
+
+const componentID = ref(`component-${crypto.randomUUID()}`);
+
+const emit = defineEmits(['update:show']);
+
+function handleOnClose() {
+    isShowingModal.value = false
+    emit('update:show', false);
+}
+
+
+</script>
+<template>
+    <OverlayBox :show="isShowingModal">
+        <template #content>
+            <div class="modal-container">
+                <div class="sidebar-container" v-if="showSidebar">
+                    <slot name="sidebar" />
+                </div>
+                <div class="main-container">
+                    <slot name="content" />
+                </div>
+            </div>
+
+            <RoundButton class="close-btn" @click="handleOnClose">
+                <template #icon>
+                    <img src="@/components/icons/close.svg" height="20px" />
+                </template>
+            </RoundButton>
+        </template>
+    </OverlayBox>
+</template>
+<style scoped>
+.modal-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    width: fit-content;
+    height: fit-content;
+}
+.main-container {
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    width: v-bind(mainWidth);
+    height: v-bind(height);
+    border-top-left-radius: v-bind(adjustableCornerRadisu);
+    border-bottom-left-radius: v-bind(adjustableCornerRadisu);
+    border-top-right-radius: 7px;
+    border-bottom-right-radius: 7px;
+    overflow-y: auto;
+    background-color: rgb(55, 55, 55);
+    color: white;
+}
+.sidebar-container {
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    width: v-bind(sidebarWidth);
+    height: v-bind(height);
+    border-top-left-radius: 7px;
+    border-bottom-left-radius: 7px;
+    border-top-right-radius: 0px;
+    border-bottom-right-radius: 0px;
+    overflow-y: auto;
+    background-color: rgb(65, 65, 65);
+    color: white;
+}
+.close-btn {
+    position: absolute;
+    bottom: 50px;
+    left: 50%;
+    transform: translateX(-50%);
+}
+</style>
--- src/views/PresentationView.vue
+++ src/views/PresentationView.vue
@@ -6,6 +6,7 @@ import Menu from '@/components/Menu.vue';
 import MenuItem from '@/components/MenuItem.vue';
 import { onMounted, ref } from 'vue';
 import OverlayBox from '@/components/OverlayBox.vue';
+import Modal from '@/components/Modal.vue';
 
 const cp = ref(0);
 const showMenu = ref(false);
@@ -15,6 +16,11 @@ onMounted(() => {
     // Initialize any necessary data or state here
     showMenu.value = false;
 });
+
+function handleShowOveralay() {
+    showOverlay.value = true;
+    console.log("showOverlay", showOverlay.value)
+}
 </script>
 <template>
     <AnchorGroup>
@@ -40,7 +46,7 @@ onMounted(() => {
                 </template>
                 <template #text>Visit Repository</template>
             </MenuItem>
-            <MenuItem @onClick="showMenu = false" @click="showOverlay = true">
+            <MenuItem @onClick="showMenu = false" @click="handleShowOveralay">
                 <template #icon>
                     <img src="@/components/icons/info.svg" height="20px" />
                 </template>
@@ -55,18 +61,13 @@ onMounted(() => {
         </template>
     </Menu>
     <NavigationBar :length="10" v-model:currentPage="cp"/>
-    <OverlayBox :show="showOverlay" :transparentcy="0.5">
+    <Modal v-model:show="showOverlay" :show-sidebar="true">
         <template #content>
-            <div style="width: 100%; height: 100%; display: flex; justify-content: center; align-items: center;">
-                <h1>Overlay Content</h1>
-                <RoundButton @click="showOverlay = false">
-                    <template #icon>
-                        <img src="@/components/icons/close.svg" height="20px" />
-                    </template>
-                </RoundButton>
-            </div>
+            <p>This is a sample presentation application.</p>
+            <p>Click the button below to close this modal.</p>
+            <RoundButton @click="showOverlay = false">Close</RoundButton>
         </template>
-    </OverlayBox>
+    </Modal>
 </template>
 <style scoped>
 </style>
