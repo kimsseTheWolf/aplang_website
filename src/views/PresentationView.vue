e4bc57123bb39570e02aa183ce6ba82b392191d1
kimsseTheWolf <ilovemcmrcreeper@gmail.com>
2025-04-23 14:38:09 -0400

    Create basic components for overlay box

--- src/views/PresentationView.vue
+++ src/views/PresentationView.vue
@@ -5,9 +5,11 @@ import RoundButton from '@/components/RoundButton.vue';
 import Menu from '@/components/Menu.vue';
 import MenuItem from '@/components/MenuItem.vue';
 import { onMounted, ref } from 'vue';
+import OverlayBox from '@/components/OverlayBox.vue';
 
 const cp = ref(0);
 const showMenu = ref(false);
+const showOverlay = ref(false);
 
 onMounted(() => {
     // Initialize any necessary data or state here
@@ -38,7 +40,7 @@ onMounted(() => {
                 </template>
                 <template #text>Visit Repository</template>
             </MenuItem>
-            <MenuItem @onClick="showMenu = false">
+            <MenuItem @onClick="showMenu = false" @click="showOverlay = true">
                 <template #icon>
                     <img src="@/components/icons/info.svg" height="20px" />
                 </template>
@@ -53,6 +55,18 @@ onMounted(() => {
         </template>
     </Menu>
     <NavigationBar :length="10" v-model:currentPage="cp"/>
+    <OverlayBox :show="showOverlay" :transparentcy="0.5">
+        <template #content>
+            <div style="width: 100%; height: 100%; display: flex; justify-content: center; align-items: center;">
+                <h1>Overlay Content</h1>
+                <RoundButton @click="showOverlay = false">
+                    <template #icon>
+                        <img src="@/components/icons/close.svg" height="20px" />
+                    </template>
+                </RoundButton>
+            </div>
+        </template>
+    </OverlayBox>
 </template>
 <style scoped>
 </style>
